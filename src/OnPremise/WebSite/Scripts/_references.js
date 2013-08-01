4fcefd5b00cded47d3bdb4c8d2617cb07407d144
Andy Brudtkuhl <abrudtkuhl@gmail.com>
2013-07-31 23:28:56 -0500

    bootstrap package

--- src/OnPremise/WebSite/Scripts/_references.js
+++ src/OnPremise/WebSite/Scripts/_references.js
@@ -1,5 +1,5 @@
-﻿/// <reference path="modernizr-2.6.2.js" />
-/// <reference path="jquery-1.8.2.js" />
+﻿/// <reference path="jquery-1.9.1.js" />
+/// <reference path="modernizr-2.6.2.js" />
 /// <reference path="jquery-ui-1.9.0.js" />
 /// <reference path="jquery.validate.js" />
 /// <reference path="jquery.validate.unobtrusive.js" />
